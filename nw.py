71c06e86b39ce305a02ce8b0c8c9bdac4f28d241
zhang46461 <149031189+zhang46461@users.noreply.github.com>
2023-10-31 18:48:46 +0800

    Create nw.py

---
+++ nw.py
@@ -0,0 +1,20 @@
+import time
+import almath
+import math
+import numpy as np
+from naoqi import ALProxy
+
+PORT=9559
+IP="192.168.12.113"
+motion = ALProxy("ALMotion",IP,PORT)
+post = ALProxy("ALRobotPosture",IP,PORT)
+post.goToPosture("Stand",0.5)
+motion.moveTo(0.5, 0, 0)
+time.sleep(1.0)
+names = ["HeadYaw","HeadPitch"]
+angleLists0 = [0 * almath.TO_RAD, 20 * almath.TO_RAD]
+timeLists = [0.5,0.5]
+motion.setStiffnesses('HeadPitch', 1.0)
+motion.setStiffnesses('HeadYaw', 1.0)
+motion.angleInterpolation(names, angleLists0, timeLists, True)
+post.goToPosture("Crouch",0.5)
